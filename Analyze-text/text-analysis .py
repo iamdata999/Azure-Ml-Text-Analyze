96adb5e25c31469ec09bf02b4d7d5ec59561d0cc
Md Nizam Sapiee <78304538+iamdata999@users.noreply.github.com>
2024-02-15 19:42:00 +0800

    Update text-analysis.py

--- Analyze-text/text-analysis .py
+++ Analyze-text/text-analysis .py
@@ -1,8 +1,8 @@
 from dotenv import load_dotenv
 import os
-
-# Import namespaces
-
+# import namespaces
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.textanalytics import TextAnalyticsClient
 
 def main():
     try:
@@ -11,8 +11,10 @@ def main():
         cog_endpoint = os.getenv('COG_SERVICE_ENDPOINT')
         cog_key = os.getenv('COG_SERVICE_KEY')
 
+        
         # Create client using endpoint and key
-
+        credential = AzureKeyCredential(cog_key)
+        cog_client = TextAnalyticsClient(endpoint=cog_endpoint, credential=credential)
 
         # Analyze each text file in the reviews folder
         reviews_folder = 'reviews'
@@ -22,25 +24,38 @@ def main():
             text = open(os.path.join(reviews_folder, file_name), encoding='utf8').read()
             print('\n' + text)
 
-            # Get language
+             # Get language
+            detectedLanguage = cog_client.detect_language(documents=[text])[0]
+            print('\nLanguage: {}'.format(detectedLanguage.primary_language.name))
 
+             # Get sentiment
+            sentimentAnalysis = cog_client.analyze_sentiment(documents=[text])[0]
+            print("\nSentiment: {}".format(sentimentAnalysis.sentiment))
 
-            # Get sentiment
+             # Get key phrases
+            phrases = cog_client.extract_key_phrases(documents=[text])[0].key_phrases
+            if len(phrases) > 0:
+             print("\nKey Phrases:")
+             for phrase in phrases:
+              print('\t{}'.format(phrase))
 
+             # Get entities
+            entities = cog_client.recognize_entities(documents=[text])[0].entities
+            if len(entities) > 0:
+             print("\nEntities")
+             for entity in entities:
+              print('\t{} ({})'.format(entity.text, entity.category))
 
-            # Get key phrases
-
-
-            # Get entities
-
-
-            # Get linked entities
-
+             # Get linked entities
+            entities = cog_client.recognize_linked_entities(documents=[text])[0].entities
+            if len(entities) > 0:
+             print("\nLinks")
+             for linked_entity in entities:
+              print('\t{} ({})'.format(linked_entity.name, linked_entity.url))
 
 
     except Exception as ex:
         print(ex)
-
 
 if __name__ == "__main__":
     main()
